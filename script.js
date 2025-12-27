4981607b4e936e36af04b119c61104643a5ec55a
Ammor <ammaro.nassero@gmail.com>
2025-12-27 22:04:52 +0800

    First upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var mainphone = document.querySelector(".img-phone")
+var allproduct = document.querySelectorAll(".allproduct")
+var maincon = document.querySelector(".first-con")
+
+
+
+
+
+allproduct.forEach(phone => {
+    phone.addEventListener("click" , () => {
+     mainphone.src = phone.src
+      maincon.style.backgroundColor= phone.dataset.color;
+    })
+   
+});
+
+
+
+// function phones(phone) {
+//     img.src = phone;
+//     img.onclick = 'phones (this.src)'
+    
+// }
